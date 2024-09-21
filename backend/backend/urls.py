6292742736008991c5baf064e111b4c8d7176ea9
amalsiva7 <amalsiva7210@gmail.com>
2024-09-21 09:29:09 +0530

    initial commit

---
+++ backend/backend/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path,include
+from api.views import CreateUserView
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/user/register',CreateUserView.as_view(),name="register"),
+    path()
+]
